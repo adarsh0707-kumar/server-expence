66ff9793f49d8bd605c251857cd729dab56038d0
Adarsh Kumar <adarshku123456789gmail.com>
2025-06-23 05:02:59 +0530

    DeleteAccount

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,15 +1,16 @@
 // import necessary modules and functions
 
-import express from 'express';
-import authMiddleware from '../middleware/authMiddleware.js';
-import { addMoneyToAccount, createAccount, getAccount } from '../controllers/accountController.js';
+import express from 'express'
+import authMiddleware from '../middleware/authMiddleware.js'
+import { addMoneyToAccount, createAccount, deleteAccount, getAccount } from '../controllers/accountController.js'
 
-const router = express.Router();
+const router = express.Router()
 
 // Define routes for account operations
 
-router.get("/:id?", authMiddleware, getAccount)
-router.post("/create", authMiddleware, createAccount);
-router.put("/add-money/:id", authMiddleware, addMoneyToAccount);
+router.get('/:id?', authMiddleware, getAccount)
+router.post('/create', authMiddleware, createAccount)
+router.put('/add-money/:id', authMiddleware, addMoneyToAccount)
+router.delete('/delete-account/:id', authMiddleware, deleteAccount)
 
-export default router;
+export default router
